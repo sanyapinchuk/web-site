42c8696fb4de63bcdd25b0543e16a468d83c4b6f
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2022-05-08 16:38:40 +0300

    add all changes

--- js/make_order.js
+++ js/make_order.js
@@ -21,3 +21,26 @@ document.querySelector("#icon-close").onclick = function(){
     elem2.style.opacity = '1';
   document.body.style.overflow = 'visible';		
 }
+function myFunction() {
+  if (document.selection) { // IE
+    var range = document.body.createTextRange();
+    range.moveToElementText(document.getElementById("mynumber"));
+    range.select();
+    document.execCommand("copy");
+} else if (window.getSelection) {
+    var range = document.createRange();
+    range.selectNode(document.getElementById("mynumber"));
+   // 
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+}
+  
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Скопировано ✔"
+}
+
+function outFunc() {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = "Копировать";
+}
