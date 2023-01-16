8d2463c4c5139ad6e69b998777a9ca9e3707abff
sanyapinchuk <sanya.pinchuk.2015@mail.ru>
2023-01-17 00:36:00 +0300

    replace slash

--- php_заменить(старый удалить)/handler.php
+++ php_заменить(старый удалить)/handler.php
@@ -1,16 +1,16 @@
 <?php
 $f = fopen("log.txt",'a');
-fwrite($f,"/r/n");
+fwrite($f,"\r\n");
 fclose($f);
 file_put_contents('log.txt', json_encode($_POST,JSON_UNESCAPED_UNICODE) . " ||  Date: " . date('Y-m-d H:i:s'), FILE_APPEND);
 $message ="";
 if($_SERVER['HTTP_REFERER']=='https://it-car.by/contact_us')
 {
     $message =
-        'name: '. $_POST['name']."/r/n".
-        'email: '.$_POST['email']."/r/n".
-        'phone: '.$_POST['phone']."/r/n".
-        'budget: '. $_POST['budget']."/r/n".
+        'name: '. $_POST['name']."\r\n".
+        'email: '.$_POST['email']."\r\n".
+        'phone: '.$_POST['phone']."\r\n".
+        'budget: '. $_POST['budget']."\r\n".
         'request: '. $_POST['request'];
 
     mail('info.itcar@gmail.com', 'Request from the user', $message);
@@ -19,10 +19,10 @@ if($_SERVER['HTTP_REFERER']=='https://it-car.by/contact_us')
 else
 {
     $message =
-        'name: '. $_POST['name']."/r/n".
-        'email: '.$_POST['email']."/r/n".
-        'phone: '.$_POST['phone']."/r/n".
-        'car/'s address: '. $_SERVER['HTTP_REFERER'];
+        'name: '. $_POST['name']."\r\n".
+        'email: '.$_POST['email']."\r\n".
+        'phone: '.$_POST['phone']."\r\n".
+        'car\'s address: '. $_SERVER['HTTP_REFERER'];
 
     mail('info.itcar@gmail.com', 'Order from the user', $message);
     header('Location: '.$_SERVER['HTTP_REFERER']);
